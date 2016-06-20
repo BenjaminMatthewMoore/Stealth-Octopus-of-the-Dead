2a441305bec51dbdcd6bc722119c74d29c8cfc91
Douglas James <bananawhore@hotmail.com>
2016-06-20 16:26:25 +1000

    Added waypoint class that contains a wait duration for waypoints -editted EnemyController so that it will check to see if it needs to wait.

--- Stealth Octopus of the Dead/Assets/Scripts/EnemyController.cs
+++ Stealth Octopus of the Dead/Assets/Scripts/EnemyController.cs
@@ -5,32 +5,53 @@ public class EnemyController : MonoBehaviour {
 
     private NavMeshAgent NavAgent;
     public GameObject[] Waypoints;
+    public GameObject CurrentWaypoint; 
 
+    private double waitDuration; 
     private int nextWaypoint;
+    private bool hasWaited; 
     private int iterator;  
 
 	// Use this for initialization
 	void Start () {
         NavAgent = GetComponent<NavMeshAgent>();
+        waitDuration = 0.0f; 
         nextWaypoint = 0;
-        iterator = 1; 
+        iterator = 1;
+        CurrentWaypoint = Waypoints[0];
+        hasWaited = true;  
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
        int waypointCount =  Waypoints.GetLength(0);
 
-        //If the navAgent doesn't have a path, send it towards the next waypoint
-        if (!NavAgent.hasPath)
+        //see if the agent is done waiting
+        if (!hasWaited && !NavAgent.hasPath)
         {
-            NavAgent.destination = Waypoints[nextWaypoint].transform.position;
-
-            //update the nextWaypoint variable
-            nextWaypoint += iterator;
-            //see if the iterator needs to be changed
-            if (nextWaypoint == 0 || nextWaypoint == (waypointCount - 1))
-                iterator *= -1; 
+            waitDuration -= Time.deltaTime;
+            if (waitDuration <= 0.0f)
+                hasWaited = true; 
         }
+        else
+        {
+            //If the navAgent doesn't have a path, send it towards the next waypoint
+            if (!NavAgent.hasPath)
+            {
+                //First let's reset our waiting stuff
+                hasWaited = false;
+                waitDuration = Waypoints[nextWaypoint].GetComponent<Waypoint>().StayDuration;
 
+                //set the destination
+                NavAgent.destination = Waypoints[nextWaypoint].transform.position;
+
+                //update the nextWaypoint variable
+                nextWaypoint += iterator;
+                //see if the iterator needs to be changed
+                if (nextWaypoint == 0 || nextWaypoint == (waypointCount - 1))
+                    iterator *= -1;
+            }
+        }
 	}
 }
