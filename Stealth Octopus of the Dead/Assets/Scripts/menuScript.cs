9315c37bf352d038416958ba9ae052140a20bd50
Dylan Marston <dylan.marston@ad.aie.edu>
2016-06-20 12:11:01 +1000

    Made menu scenes

---
+++ Stealth Octopus of the Dead/Assets/Scripts/menuScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class menuScript : MonoBehaviour {
+
+	//public Canvas quitMenu;
+	public Button startText;
+	//public Button exitText;
+
+	// Use this for initialization
+	void Start () {
+	
+		//quitMenu = quitMenu.GetComponent<Canvas> ();
+		startText = startText.GetComponent<Button> ();
+		//exitText = exitText.GetComponent<Button> ();
+		//quitMenu.enabled = false;
+	}
+	
+//	public void ExitPress() {
+//		quitMenu.enabled = true;
+//		startText.enabled = false;
+//		exitText.enabled = false;
+//	}
+
+//	public void NoPress(){
+//		quitMenu.enabled = false;
+//		startText.enabled = true;
+//		exitText.enabled = true;
+//	}
+
+	public void GoToMainMenu(){
+		SceneManager.LoadScene (0);
+	}
+	public void PlayGame(){
+		SceneManager.LoadScene (1);
+	}
+
+	public void ExitGame (){
+		Application.Quit ();
+	}
+}
