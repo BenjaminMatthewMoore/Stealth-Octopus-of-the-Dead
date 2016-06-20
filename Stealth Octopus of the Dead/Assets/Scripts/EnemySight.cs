aa25b97ad68421403d297bfc8b45868a50210e2a
Douglas James <bananawhore@hotmail.com>
2016-06-20 11:55:57 +1000

    Basic script to get enemies to detect the player obj.
    Enemy needs following structure
    [GameObject] <parent>
                                    - EnemySight
           [Light] <child> (spotlight)

---
+++ Stealth Octopus of the Dead/Assets/Scripts/EnemySight.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySight : MonoBehaviour {
+
+    //===============================
+    //Vartiable Delcarations 
+
+    public bool PlayerInSight;
+    public Vector3 lastKnownLocation;
+
+    private NavMeshAgent navAgent;
+    private Light Spotlight;
+    private GameObject Player;
+    
+
+	// Use this for initialization
+	void Start () {
+        //First find our references 
+        Player = GameObject.FindGameObjectWithTag("Player");
+        navAgent = gameObject.GetComponent<NavMeshAgent>();
+        Spotlight = gameObject.GetComponentInChildren<Light>(); 
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //See if the player is within the view of the spotlight. 
+        Vector3 direction = Player.transform.position - transform.position;
+        float angle = Vector3.Angle(direction, Spotlight.transform.forward); 
+
+        if (angle < Spotlight.spotAngle /2 )
+        {
+            //Player in sight
+            Debug.Log("Player Spotted"); 
+        }
+
+
+	}
+}
