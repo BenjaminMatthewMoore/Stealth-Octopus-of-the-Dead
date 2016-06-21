0c1090f2e597ab35f2218f810cb8900c7c0d4712
Benjamin Moore <benjamin.moore@ad.aie.edu>
2016-06-21 12:10:58 +1000

    Backstab now has a score Added a GuiScript to add kill score

---
+++ Stealth Octopus of the Dead/Assets/Scripts/GetScore.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GetScore : MonoBehaviour {
+   GameObject Player;
+   Backstab PlayerScore;
+   public Text scoreText;
+    // Use this for initialization
+    void Start () {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        scoreText.text = "0";
+        PlayerScore = Player.GetComponent<Backstab>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scoreText.text = PlayerScore.killScore.ToString();
+	}
+}
